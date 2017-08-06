9cef201346b8253801127d00a2a95a232d0eada0
Peter Jang <peter.x.jang@gmail.com>
2017-08-06 11:47:47 -0500

    Add API security using email and api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+
+  before_action :restrict_access
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, options|
+      User.find_by(api_key: api_key, email: request.headers["X-User-Email"])
+    end
+  end
 end
